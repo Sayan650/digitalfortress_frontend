24d2ceb85d0aea0df0d57d0804150fd69f082b2f
Sayan650 <sayan650@gmail.com>
2024-01-21 22:37:38 +0530

    updated the gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,17 +6,35 @@
 
 // You can delete this file if you're not using it
 
-exports.onCreateWebpackConfig = ({ actions, stage }) => {
-    if (stage === "build-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /mapbox-gl/,
-              use: ['null-loader']
-            },
-          ],
-        }
-      })
-    }
+// exports.onCreateWebpackConfig = ({ actions, stage }) => {
+//     if (stage === "build-html") {
+//       actions.setWebpackConfig({
+//         module: {
+//           rules: [
+//             {
+//               test: /mapbox-gl/,
+//               use: ['null-loader']
+//             },
+//           ],
+//         }
+//       })
+//     }
+//   };
+
+  exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
+    const config = getConfig();
+  
+    config.module.rules = [
+      ...config.module.rules.filter(rule => String(rule.test) !== String(/\.jsx?$/)),
+      {
+        test: /maplibre-gl/,
+        use: loaders.null(),
+      },
+      {
+        test: /\.jsx?$/,
+        use: { ...loaders.js() },
+        exclude: modulePath => /node_modules/.test(modulePath),
+      },
+    ];
+  
   };
